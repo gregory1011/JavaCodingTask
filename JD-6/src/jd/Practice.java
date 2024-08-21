6ca8fbbeaaec056d6ed04684e2cb2088d1bd289a
grigorerosca <gregory081093@gmail.com>
2024-08-21 10:55:38 -0400

    Practice

---
+++ JD-6/src/jd/Practice.java
@@ -0,0 +1,21 @@
+package jd;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+
+
+        Function<List<String>, Integer> mo= s -> Math.toIntExact(s.size());
+
+
+
+
+    }
+
+
+
+}
