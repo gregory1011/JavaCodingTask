0130205680773db2ea693138b902906a606eede9
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2025-03-12 20:46:06 -0400

    Node

---
+++ JD-6/src/practice/pro/review_dataStructureAndAlgorithms/LinkedLists/Node.java
@@ -0,0 +1,14 @@
+package practice.pro.review_dataStructureAndAlgorithms.LinkedLists;
+
+
+// I can call this class= Dog
+public class Node {
+
+    int id;
+    // Dog next
+    Node next;
+
+    public Node(int id) {
+        this.id = id;
+    }
+}
