94a249355b79d155366f87c60e35261a0aa85acf
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2025-03-24 19:39:20 -0400

    RecursiveFibonacci

---
+++ JD-6/src/practice/pro/review_dataStructureAndAlgorithms/Stacks/RecursiveFibonacci.java
@@ -0,0 +1,17 @@
+package practice.pro.review_dataStructureAndAlgorithms.Stacks;
+
+public class RecursiveFibonacci {
+
+    public static void main(String[] args) {
+        //fibonacci -> 1,2,3,5,8,13,21,34,55,89,144
+        System.out.println(fibonacci(12));
+    }
+    //a typical factorial calculation will be -> n! = n*(n-1)!;
+
+    //Factorial calculation: F(n)= F(n-1) + f(n-2);
+    public static int fibonacci(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
