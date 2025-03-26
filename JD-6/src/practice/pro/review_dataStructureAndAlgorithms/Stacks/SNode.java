075d82a0c72055b86f0c4d674515d8bc2a93858c
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2025-03-26 19:57:42 -0400

    SNode

---
+++ JD-6/src/practice/pro/review_dataStructureAndAlgorithms/Stacks/SNode.java
@@ -0,0 +1,11 @@
+package practice.pro.review_dataStructureAndAlgorithms.Stacks;
+
+public class SNode<T> {
+    public T data; // data type of Generic
+    SNode next; //
+
+    public SNode(T data) {
+        this.data = data;
+    }
+
+}
